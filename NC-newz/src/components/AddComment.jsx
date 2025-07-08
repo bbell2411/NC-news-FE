fa7b5d120e40165e8fe8abbabc18116c876e3fa8
bell <bellelm@Belkises-MacBook-Air.local>
2025-07-08 19:52:13 +0100

    post request for adding comment 404

---
+++ NC-newz/src/components/AddComment.jsx
@@ -0,0 +1,35 @@
+import { useContext, useState } from "react";
+import { IoSendOutline } from "react-icons/io5";
+import { useParams } from "react-router-dom";
+import { UserContext } from "../../Contexts/User";
+import { postComment } from "./api";
+
+export const AddComment = ({ addedComment, setAddedComment }) => {
+    const [post, setPost]= useState({})
+    
+    const { article_id } = useParams()
+    const { loggedInUser } = useContext(UserContext)
+
+    const handleInputChange = (e) => {
+        setAddedComment(e.target.value)
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        postComment(article_id, addedComment, loggedInUser)
+            .then((postedComment) => {
+                // console.log(postedComment,"inside add comment")
+                setPost(postedComment)
+                setAddedComment("")
+            })
+        
+
+    }
+    console.log(addedComment,"added")
+    console.log(post,"loggin post")
+
+    return <form onSubmit={handleSubmit}>
+        <input  id="comment-input" value={addedComment} onChange={handleInputChange} type='text' placeholder="add a comment" >
+        </input>  &nbsp;
+        {addedComment !== '' ? <button type="submit"><IoSendOutline /></button> : null}
+    </form>
+}
