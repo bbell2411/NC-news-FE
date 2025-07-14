8f1c1ca8e4251e9fe78fdc96b7a3096e559764d3
bell <bellelm@Mac.lan>
2025-07-14 14:40:41 +0100

    made topics component

--- NC-newz/src/components/ViewTopics.jsx
+++ NC-newz/src/components/ViewTopics.jsx
@@ -1,3 +1,41 @@
+import { useEffect, useState } from "react"
+import { getTopics } from "./api"
+
 export const ViewTopics = () => {
-  
+    const [topics, setTopics] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+    const [isError, setIsError] = useState(null)
+
+    useEffect(() => {
+        setIsLoading(true)
+        getTopics()
+            .then((topics) => {
+                setTopics(topics)
+            })
+            .catch((err) => {
+                setError(err)
+                setIsLoading(false)
+            })
+            .finally(() => {
+                setIsLoading(false)
+            })
+    }, [])
+
+    if (isLoading) return <div className="loader-container"><h1 className="loader"></h1></div>
+
+    if (isError) return <p className="error">something went wrong</p>
+
+    return(
+        <ul className="topics-list">
+            {topics.map((topic) => {
+                return (
+                    <li className="topic-item" key={topic.slug}>
+                        <p className="topic-name">{topic.slug}</p>
+                        <p className="topic-description">{topic.description}</p>
+                        <img className="topic-image" src={topic.img_url} alt={`${topic.slug} icon`} />
+                    </li>
+                )
+            })}
+        </ul>
+    )
 }
